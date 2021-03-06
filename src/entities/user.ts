8af8d86f1a2984b9aeeff91c57b4926f9f8489f9
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-06 15:26:20 -0300

    ✨ decorate user entity with graphql

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -6,26 +6,36 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-@Entity({ name: 'users' })
+import { Field, HideField, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+@Entity()
 export class User {
+  @Field()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   name: string;
 
+  @Field()
   @Column({ unique: true })
   email: string;
 
+  @HideField()
   @Column()
   password: string;
 
+  @Field()
   @Column()
   role: string;
 
+  @Field()
   @CreateDateColumn()
   createdAt: Date;
 
+  @Field()
   @UpdateDateColumn()
   updatedAt: Date;
 }
