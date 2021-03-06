adab5da62b129c402519f8e732cd99f4f92b1859
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-06 15:23:33 -0300

    🐛 fix entities and migrations path

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -2,15 +2,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 import { DATABASE } from '@environments/database';
-import { join } from 'path';
+import { resolve } from 'path';
 
 const typeOrmOptions: TypeOrmModuleOptions = {
   ...DATABASE,
   type: 'postgres',
   synchronize: true,
   namingStrategy: new SnakeNamingStrategy(),
-  entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
-  migrations: [join(__dirname, '..', '**', 'migrations', '*.{ts,js}')],
+  entities: [resolve(__dirname, '..', 'entities', '*.{ts,js}')],
+  migrations: [resolve(__dirname, '..', 'migrations', '*.{ts,js}')],
   cli: {
     entitiesDir: 'src/entities',
     migrationsDir: 'src/migrations',
