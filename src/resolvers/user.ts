d4701f7c9662ebde51ca160042257a7c7d041cc2
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-09 17:32:58 -0300

    ♻️ only admins can manager and visualize users

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -3,21 +3,26 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { User } from '@entities/user';
 import { UserService } from '@services/user';
 import UserInput from '@inputs/user';
+import { UseGuards } from '@nestjs/common';
+import { RoleGuard } from 'src/guards/role';
 
 @Resolver('User')
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(RoleGuard)
   @Query(() => [User])
   async listUsers() {
     return await this.userService.listUsers();
   }
 
+  @UseGuards(RoleGuard)
   @Mutation(() => User)
   async createUser(@Args('data') data: UserInput): Promise<User> {
     return await this.userService.createUser(data);
   }
 
+  @UseGuards(RoleGuard)
   @Mutation(() => Boolean)
   async deleteUser(@Args('data') data: number): Promise<boolean> {
     return await this.userService.deleteUser(data);
